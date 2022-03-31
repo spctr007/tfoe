5e42aeffb747134f3f8a3d54fbe0b866643182a4
Geoffrey Eslava <geoffrey.eslava@gmail.com>
2022-03-31 22:10:03 +0800

    Added react-chat-widget.

--- src/contactUsComponent/ContactUs.tsx
+++ src/contactUsComponent/ContactUs.tsx
@@ -1,117 +1,122 @@
 import React from "react";
+import {Widget} from "react-chat-widget";
+import 'react-chat-widget/lib/styles.css'
 
 const ContactUs = () => {
-  return (
-    <section className="page-section" id="contact" style={{
-      backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/img/map-image.png'})`
-    }}>
-      <div className="container">
-        <div className="text-center">
-          <h2 className="section-heading text-uppercase">Contact Us</h2>
-          <h3 className="section-subheading text-muted">
-            Lorem ipsum dolor sit amet consectetur.
-          </h3>
-        </div>
-        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-          <div className="row align-items-stretch mb-5">
-            <div className="col-md-6">
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  id="name"
-                  type="text"
-                  placeholder="Your Name *"
-                  data-sb-validations="required"
-                />
-                <div
-                  className="invalid-feedback"
-                  data-sb-feedback="name:required"
-                >
-                  A name is required.
+    return (
+        <section className="page-section" id="contact" style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/img/map-image.png'})`
+        }}>
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Contact Us</h2>
+                    <h3 className="section-subheading text-muted">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </h3>
                 </div>
-              </div>
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  id="email"
-                  type="email"
-                  placeholder="Your Email *"
-                  data-sb-validations="required,email"
-                />
-                <div
-                  className="invalid-feedback"
-                  data-sb-feedback="email:required"
-                >
-                  An email is required.
-                </div>
-                <div
-                  className="invalid-feedback"
-                  data-sb-feedback="email:email"
-                >
-                  Email is not valid.
-                </div>
-              </div>
-              <div className="form-group mb-md-0">
-                <input
-                  className="form-control"
-                  id="phone"
-                  type="tel"
-                  placeholder="Your Phone *"
-                  data-sb-validations="required"
-                />
-                <div
-                  className="invalid-feedback"
-                  data-sb-feedback="phone:required"
-                >
-                  A phone number is required.
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-group form-group-textarea mb-md-0">
+                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                    <div className="row align-items-stretch mb-5">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    id="name"
+                                    type="text"
+                                    placeholder="Your Name *"
+                                    data-sb-validations="required"
+                                />
+                                <div
+                                    className="invalid-feedback"
+                                    data-sb-feedback="name:required"
+                                >
+                                    A name is required.
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Your Email *"
+                                    data-sb-validations="required,email"
+                                />
+                                <div
+                                    className="invalid-feedback"
+                                    data-sb-feedback="email:required"
+                                >
+                                    An email is required.
+                                </div>
+                                <div
+                                    className="invalid-feedback"
+                                    data-sb-feedback="email:email"
+                                >
+                                    Email is not valid.
+                                </div>
+                            </div>
+                            <div className="form-group mb-md-0">
+                                <input
+                                    className="form-control"
+                                    id="phone"
+                                    type="tel"
+                                    placeholder="Your Phone *"
+                                    data-sb-validations="required"
+                                />
+                                <div
+                                    className="invalid-feedback"
+                                    data-sb-feedback="phone:required"
+                                >
+                                    A phone number is required.
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group form-group-textarea mb-md-0">
                 <textarea
-                  className="form-control"
-                  id="message"
-                  placeholder="Your Message *"
-                  data-sb-validations="required"
+                    className="form-control"
+                    id="message"
+                    placeholder="Your Message *"
+                    data-sb-validations="required"
                 />
-                <div
-                  className="invalid-feedback"
-                  data-sb-feedback="message:required"
-                >
-                  A message is required.
-                </div>
-              </div>
+                                <div
+                                    className="invalid-feedback"
+                                    data-sb-feedback="message:required"
+                                >
+                                    A message is required.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-none" id="submitSuccessMessage">
+                        <div className="text-center text-white mb-3">
+                            <div className="fw-bolder">Form submission successful!</div>
+                            To activate this form, sign up at
+                            <br/>
+                            <a href="https://startbootstrap.com/solution/contact-forms">
+                                https://startbootstrap.com/solution/contact-forms
+                            </a>
+                        </div>
+                    </div>
+                    <div className="d-none" id="submitErrorMessage">
+                        <div className="text-center text-danger mb-3">
+                            Error sending message!
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <button
+                            className="btn btn-primary btn-xl text-uppercase disabled"
+                            id="submitButton"
+                            type="submit"
+                        >
+                            Send Message
+                        </button>
+                    </div>
+                </form>
             </div>
-          </div>
-          <div className="d-none" id="submitSuccessMessage">
-            <div className="text-center text-white mb-3">
-              <div className="fw-bolder">Form submission successful!</div>
-              To activate this form, sign up at
-              <br />
-              <a href="https://startbootstrap.com/solution/contact-forms">
-                https://startbootstrap.com/solution/contact-forms
-              </a>
+            <div>
+                <Widget title="Hello Kuya/Ate!" subtitle="What can I help you with?"/>
             </div>
-          </div>
-          <div className="d-none" id="submitErrorMessage">
-            <div className="text-center text-danger mb-3">
-              Error sending message!
-            </div>
-          </div>
-          <div className="text-center">
-            <button
-              className="btn btn-primary btn-xl text-uppercase disabled"
-              id="submitButton"
-              type="submit"
-            >
-              Send Message
-            </button>
-          </div>
-        </form>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default ContactUs;
